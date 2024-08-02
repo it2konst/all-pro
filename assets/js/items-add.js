48e81f43a8a07160c1adac82b7655e99e41741da
Konstantin <163849606+it2konst@users.noreply.github.com>
2024-08-02 18:15:43 +0700

    feat: add new proj (es-lp)

--- assets/js/items-add.js
+++ assets/js/items-add.js
@@ -3,6 +3,16 @@
 
 const cardsContainer = document.querySelector('.portfolio__tab-cards');
 
+// Объект для соответствия технологий и классов иконок
+// const technologyIcons = {
+//     html: 'fa-html5',
+//     css: 'fa-css3-alt',
+//     JS: 'fa-js',
+//     jquery: 'fa-jquery',
+//     WordPress: 'fa-wordpress',
+//     PHP: 'fa-php'
+// };
+
 // Функция для создания элемента
 const createCard = (myCard) => {
     const cardEl = document.createElement('li');
@@ -11,13 +21,60 @@ const createCard = (myCard) => {
     cardEl.addEventListener('click', () => {
         window.open(myCard.onclick, '_blank');
     });
-    cardEl.innerHTML = `
+
+    // Начало формирования HTML-контента
+    let cardContent = `
         <div class="card-el">
-            <img src="${myCard.imgSrc}" alt="${myCard.alt}">
-            <h2 class="card-title">${myCard.cardTitle}</h2>
-            <p>${myCard.cardDesc}</p>
-        </div> 
-    `;
+            <div class="card-images">
+                <img src="${myCard.imgSrc}" alt="${myCard.alt}">
+                <div class="card-icons">`
+
+    // Добавление иконок, если поле "technologies" присутствует
+    if (myCard.hasOwnProperty('technologies')) {
+        myCard.technologies.forEach((technology) => {
+            if (technology === 'html') {
+                cardContent += `
+                    <div class="icon-box">
+                        <i class="fa-brands fa-html5"></i>
+                    </div>`;
+            }
+            if (technology === 'scss') {
+                cardContent += `
+                    <div class="icon-box">
+                        <i class="fa-brands fa-sass"></i>
+                    </div>`;
+            }
+            if (technology === 'JS') {
+                cardContent += `
+                    <div class="icon-box">
+                        <i class="fa-brands fa-js"></i>
+                    </div>`;
+            }
+            if (technology === 'WordPress') {
+                cardContent += `
+                    <div class="icon-box">
+                        <i class="fa-brands fa-wordpress"></i>
+                    </div>`;
+            }
+            if (technology === 'PHP') {
+                cardContent += `
+                    <div class="icon-box">
+                        <i class="fa-brands fa-php"></i>
+                    </div>`;
+            }
+        });
+    };
+
+    // Завершение формирования HTML-контента
+    cardContent += `
+                </div>
+            </div>
+                <h2 class="card-title">${myCard.cardTitle}</h2>
+                <p>${myCard.cardDesc}</p>
+        </div>`;
+
+    // Присваивание HTML-контента элементу
+    cardEl.innerHTML = cardContent;
     return cardEl;
 };
 

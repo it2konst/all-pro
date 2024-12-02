42ed77bccaf33605c381163ca6d425d38b7891e7
Konstantin <163849606+it2konst@users.noreply.github.com>
2024-12-02 21:12:49 +0700

    add new pro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,14 @@
-const loadScript = (src) => new Promise((resolve, reject) => {
-    const script = document.createElement('script');
-    script.src = src;
-    script.onload = () => resolve(src);
-    script.onerror = () => reject(new Error(`Failed to load script ${src}`));
-    document.head.appendChild(script);
-});
+const loadScript = (src) =>
+    new Promise((resolve, reject) => {
+        const script = document.createElement("script");
+        script.src = src;
+        script.onload = () => resolve(src);
+        script.onerror = () => reject(new Error(`Failed to load script ${src}`));
+        document.head.appendChild(script);
+    });
 
 const loadScripts = async () => {
-    const scripts = [
-        './assets/js/items-add.js',
-        './assets/js/nav-menu.js',
-        './assets/js/libs/jquery-3.1.1.min.js',
-        './assets/js/libs/sort-items.js'
-    ];
+    const scripts = ["./assets/js/items-add.js", "./assets/js/nav-menu.js", "./assets/js/libs/jquery-3.1.1.min.js", "./assets/js/libs/sort-items.js"];
 
     try {
         for (const script of scripts) {
@@ -20,7 +16,7 @@ const loadScripts = async () => {
             console.log(`Script ${script} loaded`);
         }
     } catch (error) {
-        console.error('Error loading scripts:', error);
+        console.error("Error loading scripts:", error);
     }
 };
 
